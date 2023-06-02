051fc0ef770472bf37d1243754ed6def4ad9cec4
faithmwamkinga <faithmwamkinga@gmail.com>
2023-06-03 00:09:14 +0300

    Fetching products from a dummy site fo the websites

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-// https://dummyjson.com/products
-function generateProductCards(data) {
-    const productContainer = document.getElementById("product-container");
-    // iterate through the data and create a card for each product
-    data.forEach((product) => {
-      // create a card element
-      const card = document.createElement("div");
-      card.classList.add("product-card");
-      // Set the card content using the product data
-      card.innerHTML = `
-              <img src="${product.thumbnail}" alt="${product.name}" />
-              <h2>${product.title}</h2>
-              <div class='productPrice'>
-              <p>${product.price}</p>
-              <button>ADD TO CART</button>
-              </div>
-          `;
-      // append the card to the product container
-      productContainer.appendChild(card);
-    });
-  }
-  function fetchProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((data) => {
-        // Get the first 8 products from the data
-        const returnedData = data.products.slice(0, 8);
-        // call the function to generate cards
-        generateProductCards(returnedData);
-      });
-  }
-  fetchProducts()
+const productContainer=document.getElementById("products")
+const getProducts=()=>{
+  return fetch("https://dummyjson.com/products?limit=8")
+  .then(response=>response.json())
+  .then(response=>response)
+  .catch(error=>error)
+}
+
+
+const displayProducts=async()=>{
+  const products=await getProducts();
+  console.log(products);
+  products.products.map(item=>{
+    let div=document.createElement("div")
+    let image=document.createElement("img")
+    let price=document.createElement("p")
+    let discountPercentage=document.createElement("p")
+    let rating=document.createElement("p")
+    let stock=document.createElement("p")
+    let brand=document.createElement("p")
+    let category=document.createElement("p")
+
+    image.src=item.thumbnail
+    price.innerHTML=item.price
+    discountPercentage.innerHTML=item.discount
+    rating.innerHTML=item.rating
+    stock.innerHTML=item.stock
+    brand.innerHTML=item.brand
+    category.innerHTML=item.category
+
+    div.appendChild(image)
+    div.appendChild(price)
+    div.appendChild(discountPercentage)
+    div.appendChild(rating)
+    div.appendChild(stock)
+    div.appendChild(brand)
+    div.appendChild(category)
+    div.id='key'
+    div.setAttribute('class','product')
+    productContainer.appendChild(div)
+  })
+}
+displayProducts();
